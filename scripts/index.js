5cb30b68b67701a792b43a80439efcb35d98e991
宫 酉昊 <gongtorikou@Gong-Youhao.local>
2015-10-25 01:19:04 +0800

    asdf

--- scripts/index.js
+++ scripts/index.js
@@ -11,31 +11,31 @@ $("#contact-submit").click(function(){
 
 	if (name == null || name.length == 0){
 		alert("姓名为空！");
-		// return;
+		return false;
 	}
 	if (number == null || number.length == 0){
 		alert("学号为空！");
-		// return;
+		return false;
 	}
 	if (college == null || college.length == 0){
 		alert("学院为空！");
-		// return;
+		return false;
 	}
 	if (phone == null || phone.length == 0){
 		alert("手机为空！");
-		// return;
+		return false;
 	}
 	if (email == null || email.length == 0){
 		alert("邮箱为空！");
-		// return;
+		return false;
 	}
 	if (introduction == null || introduction.length == 0){
 		alert("个人介绍为空！");
-		// return;
+		return false;
 	}
 	if (introduction.length > 200){
 		alert("个人介绍超过200字！");
-		// return;
+		return false;
 	}
 
 	alert("姓名：" + name + "    " + "性别(0男/1女)：" + gender + "    " + "学号：" + number + "    " + "学院：" + college + "    " + "手机：" + phone + "    " + "邮箱：" + email + "    " + "实验室(0,1,2,3)：" + labType + "    " + "是否接受调剂(0否/1是)：" + deferToDistributionType + "    " + "个人介绍：" + introduction);
