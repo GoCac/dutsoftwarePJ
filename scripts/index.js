795fa0891d265a20737de7c6dd74f0bb005c2e63
宫 酉昊 <gongtorikou@Gong-Youhao.local>
2015-10-25 01:09:13 +0800

    s

--- scripts/index.js
+++ scripts/index.js
@@ -11,34 +11,34 @@ $("#contact-submit").click(function(){
 
 	if (name == null || name.length() == 0){
 		alert("姓名为空！");
-		// return;
+		return;
 	}
 	if (number == null || number.length() == 0){
 		alert("学号为空！");
-		// return;
+		return;
 	}
 	if (college == null || college.length() == 0){
 		alert("学院为空！");
-		// return;
+		return;
 	}
 	if (phone == null || phone.length() == 0){
 		alert("手机为空！");
-		// return;
+		return;
 	}
 	if (email == null || email.length() == 0){
 		alert("邮箱为空！");
-		// return;
+		return;
 	}
 	if (introduction == null || introduction.length() == 0){
 		alert("个人介绍为空！");
-		// return;
+		return;
 	}
 	if (introduction.length() > 200){
 		alert("个人介绍超过200字！");
-		// return;
+		return;
 	}
 
-	alert("姓名：" + name + "\n性别(0男/1女)：" + gender + "\n学号：" + number + "\n学院：" + college + "\n手机：" + phone + "\n邮箱：" + email + "\n实验室(0,1,2,3)：" + labType + "\n是否接受调剂(0否/1是)：" + deferToDistributionType + "\n个人介绍：" + introduction);
+	alert("姓名：" + name + "    " + "性别(0男/1女)：" + gender + "    " + "学号：" + number + "    " + "学院：" + college + "    " + "手机：" + phone + "    " + "邮箱：" + email + "    " + "实验室(0,1,2,3)：" + labType + "    " + "是否接受调剂(0否/1是)：" + deferToDistributionType + "    " + "个人介绍：" + introduction);
 	/*
 	$.get("index.php/positions/data",{"dataType":"json",icao:micao},function(data,status){
 	obj=eval(data);
