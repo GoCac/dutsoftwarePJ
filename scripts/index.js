9ffc88586026a897af6236a2f5545d3dade54f69
宫 酉昊 <gongtorikou@Gong-Youhao.local>
2015-10-24 21:26:30 +0800

    s

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 $("#contact-submit").click(function(){
-				var micao=$("#name").val();
-				alert(micao);
-				/*
-				$.get("index.php/positions/data",{"dataType":"json",icao:micao},function(data,status){
-					obj=eval(data);
-					*/
-				// $.get("data",{"dataType":"json",icao:micao},function(data,status){
-				// 	obj=eval(data);
-				// deleteOverlays();
-				// $(obj).each(function(i){
-				// 	markers[i]=new qq.maps.Marker({
-				// 		position: new qq.maps.LatLng(obj[i]["lat"],obj[i]["lon"]),
-				// 		map: map
-				// 	});
-				// });
-			 //  });
+	var micao=$("#name").val();
+	alert(micao);
+	/*
+	$.get("index.php/positions/data",{"dataType":"json",icao:micao},function(data,status){
+	obj=eval(data);
+	*/
+	// $.get("data",{"dataType":"json",icao:micao},function(data,status){
+	// 	obj=eval(data);
+	// 	deleteOverlays();
+	// 	$(obj).each(function(i){
+	// 		markers[i]=new qq.maps.Marker({
+	// 			position: new qq.maps.LatLng(obj[i]["lat"],obj[i]["lon"]),
+	// 			map: map
+	// 		});
+	// 	});
+	// });
 });
