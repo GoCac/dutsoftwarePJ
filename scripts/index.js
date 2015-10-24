ce3d14af5e2ba6201bf6610021365ccdf54c6843
宫 酉昊 <gongtorikou@Gong-Youhao.local>
2015-10-24 23:59:09 +0800

    d

--- scripts/index.js
+++ scripts/index.js
@@ -13,14 +13,14 @@ $("#contact-submit").click(function(){
 	$.get("index.php/positions/data",{"dataType":"json",icao:micao},function(data,status){
 	obj=eval(data);
 	*/
-	// $.get("data",{"dataType":"json",icao:micao},function(data,status){
-	// 	obj=eval(data);
-	// 	deleteOverlays();
-	// 	$(obj).each(function(i){
-	// 		markers[i]=new qq.maps.Marker({
-	// 			position: new qq.maps.LatLng(obj[i]["lat"],obj[i]["lon"]),
-	// 			map: map
-	// 		});
-	// 	});
-	// });
+	$.get("http://dutse.avosapps.com/student/update",{"name":name,"gender":gender,"number":number,"college":college,"phone":phone,"email":email,"labType":labType,"deferToDistributionType":deferToDistributionType,"introduction":introduction,},function(data,status){
+		// obj=eval(data);
+		// deleteOverlays();
+		// $(obj).each(function(i){
+		// 	markers[i]=new qq.maps.Marker({
+		// 		position: new qq.maps.LatLng(obj[i]["lat"],obj[i]["lon"]),
+		// 		map: map
+		// 	});
+		// });
+	});
 });
