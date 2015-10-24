27c5c40503d19cdb0a9e2de1a896503382d37307
宫 酉昊 <gongtorikou@Gong-Youhao.local>
2015-10-24 20:27:15 +0800

    jQuery

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+$("#contact-submit").click(function(){
+				var micao=$("#text").attr("value");
+				alert(micao);
+				/*
+				$.get("index.php/positions/data",{"dataType":"json",icao:micao},function(data,status){
+					obj=eval(data);
+					*/
+				// $.get("data",{"dataType":"json",icao:micao},function(data,status){
+				// 	obj=eval(data);
+				// deleteOverlays();
+				// $(obj).each(function(i){
+				// 	markers[i]=new qq.maps.Marker({
+				// 		position: new qq.maps.LatLng(obj[i]["lat"],obj[i]["lon"]),
+				// 		map: map
+				// 	});
+				// });
+			 //  });
+});
